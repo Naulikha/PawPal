9d1e2b65575d30de8611c22eb7bc76469a84fea7
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 05:53:27 +0000

    Added initial game loop without fully implemented renderer

--- PawPal/src/pawpall/engine/GameLoop.java
+++ PawPal/src/pawpall/engine/GameLoop.java
@@ -1,5 +1,27 @@
 package pawpall.engine;
 
-public class GameLoop {
+//using animation timer to sync with monitors refresh rate 
+import javafx.animation.AnimationTimer;
+import pawpall.rendering.Renderer;
 
+public class GameLoop extends AnimationTimer {
+    private Renderer renderer;
+
+    public GameLoop(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    @Override
+    public void handle(long now) {
+        update();
+        render();
+    }
+
+    private void update() {
+        // Future: Update physics and input here
+    }
+
+    private void render() {
+        renderer.draw();
+    }
 }
