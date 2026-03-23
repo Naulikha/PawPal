983d7819fa857f0238b5d125dff2582e603f0a88
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 05:37:48 +0000

    implemented testVectorAddition (passing)

--- PawPal/src/pawpall/util/Vector2.java
+++ PawPal/src/pawpall/util/Vector2.java
@@ -1,5 +1,18 @@
 package pawpall.util;
 
+/**
+ * Basic 2D Vector class for positioning and velocity.
+ * Part of Milestone 1: Basic design and functionalities.
+ */
 public class Vector2 {
+    public double x, y;
 
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2 add(Vector2 other) {
+        return new Vector2(this.x + other.x, this.y + other.y);
+    }
 }
