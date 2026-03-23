5c19e7c31f287aa1f0a703360ede5d9a7691c5ad
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 06:25:55 +0000

    Created gameTest for movement (failing)

--- PawPal/test/pawpall/engine/GameTest.java
+++ PawPal/test/pawpall/engine/GameTest.java
@@ -1,5 +1,26 @@
 package pawpall.engine;
 
-public class GameTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import javafx.scene.input.KeyCode;
+import pawpall.player.Player;
+import pawpall.util.Vector2;
 
+public class GameTest {
+    
+    @Test
+    void testCatMovesRightOnDKey() {
+        // This is a "Mock-like" setup for TDD
+        // We will implement a concrete version of Game to test it
+        Game testGame = new GameImplementation(); 
+        
+        double initialX = testGame.cat.getPosition().x;
+        
+        // Simulate 'D' key being pressed (this requires implementation logic)
+        // For now, this test will fail because handleInput() is empty
+        testGame.handleInput(); 
+        
+        // We expect X to increase if 'D' is pressed
+        assertTrue(testGame.cat.getPosition().x > initialX, "Cat should move right");
+    }
 }
