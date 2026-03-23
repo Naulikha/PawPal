b76ded0a56aab1679c27e72252928d0f1af282a6
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 06:06:02 +0000

    Created testPlayerMovement and testDogCanPushandCatCannot (failing)

--- PawPal/test/pawpall/player/PlayerTest.java
+++ PawPal/test/pawpall/player/PlayerTest.java
@@ -1,42 +1,28 @@
-//package pawpall.player;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//
-//public class PlayerTest {
-//	
-//	// Test - X should increase by given Speed when moving right
-//	@Test
-//    public void testMoveRight() {
-//        Player player = new Player(0, 0, 5, -15);
-//        player.moveRight();
-//        assertTrue(player.getVelocityX() > 0);
-//    }
-//
-//	// Test - X should decrease by given Speed when moving left
-//	@Test
-//	public void testMoveLeft() {
-//	    Player player = new Player(0, 0, 5, -15);
-//	    player.moveLeft();
-//	    assertTrue(player.getVelocityX() < 0);
-//	}
-//	
-//	// Test - VelocityX should be 0 when it stops
-//	@Test
-//	public void playerStopsMoving() {
-//	    Player player = new Player(0, 0, 5, -15);
-//	    player.moveRight();
-//	    player.stop();
-//	    assertEquals(0, player.getVelocityX());
-//	}
-//	
-//
-//	// Test - Jumping should set a negative vertical velocity (as negative Y is Up).
-//	@Test
-//    void testJump() {
-//        Player player = new Player(0, 0, 5, -15);
-//        player.jump();
-//        assertTrue(player.getVelocityY() < 0);
-//    }
-//	
-//}
+package pawpall.player;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import pawpall.util.Vector2;
+
+public class PlayerTest {
+    @Test
+    void testPlayerMovement() {
+        Player cat = new Player(Player.Type.CAT, new Vector2(0, 0));
+        Vector2 velocity = new Vector2(5, 10);
+        
+        // We want to test a move method we haven't written yet
+        cat.move(velocity); 
+        
+        assertEquals(5.0, cat.getPosition().x, 0.001);
+        assertEquals(10.0, cat.getPosition().y, 0.001);
+    }
+
+    @Test
+    void testDogCanPushAndCatCannot() {
+        Player dog = new Player(Player.Type.DOG, new Vector2(0, 0));
+        Player cat = new Player(Player.Type.CAT, new Vector2(0, 0));
+        
+        assertTrue(dog.canPushObjects(), "Dog should be able to push boulders ");
+        assertFalse(cat.canPushObjects(), "Cat should not be able to push boulders ");
+    }
+}
