99fab5a4340756609a1dcfb411a2ff727e90ca24
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 06:18:16 +0000

    Created abstract methods for game logic

--- PawPal/src/pawpall/engine/Game.java
+++ PawPal/src/pawpall/engine/Game.java
@@ -1,5 +1,18 @@
 package pawpall.engine;
 
-public class Game {
+import pawpall.input.InputHandler;
+import pawpall.player.Player;
 
+/**
+ * High-level engine logic. 
+ * Defined first as per TDD Step 1.
+ */
+public abstract class Game {
+    protected Player cat;
+    protected Player dog;
+    protected InputHandler inputHandler;
+
+    // Abstract method to be implemented after testing
+    public abstract void handleInput();
+    public abstract void update();
 }
