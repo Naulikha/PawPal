ee00891ef22a378f6cb5f958fec5c02be61cfb4b
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 20:47:11 +0000

    Created PhysicsEngineTest (failing)

--- PawPal/test/pawpall/physics/PhysicsEngineTest.java
+++ PawPal/test/pawpall/physics/PhysicsEngineTest.java
@@ -1,5 +1,21 @@
 package pawpall.physics;
 
-public class PhysicsEngineTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import pawpall.player.Player;
+import pawpall.util.Vector2;
 
+public class PhysicsEngineTest {
+    @Test
+    void testWallCollision() {
+        // We will create a concrete detector to test
+        CollisionDetector detector = new CollisionDetector();
+        Player cat = new Player(Player.Type.CAT, new Vector2(490, 100));
+        
+        // Try to move into a wall at x = 500
+        Vector2 nextPos = new Vector2(510, 100);
+        
+        // This should return true (colliding)
+        assertTrue(detector.isColliding(cat, nextPos), "Player should collide with the boundary at 500");
+    }
 }
