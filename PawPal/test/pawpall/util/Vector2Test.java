17097960aca577c68f4991f9500a96421c42c873
Naulikha Austin <nbiketia@gmail.com>
2026-03-23 05:35:33 +0000

    Created testVectorAddition (failing)

--- PawPal/test/pawpall/util/Vector2Test.java
+++ PawPal/test/pawpall/util/Vector2Test.java
@@ -1,5 +1,19 @@
 package pawpall.util;
 
-public class Vector2Test {
+//imports 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+//every movement for cat and dog depends on coordinate math 
+
+public class Vector2Test {
+    @Test
+    void testVectorAddition() {
+        Vector2 v1 = new Vector2(10, 20);
+        Vector2 v2 = new Vector2(5, 5);
+        Vector2 result = v1.add(v2);
+        // We use a delta of 0.001 for floating point precision
+        assertEquals(15.0, result.x, 0.001);
+        assertEquals(25.0, result.y, 0.001);
+    }
 }
